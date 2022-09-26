e281256216afda4ad6a67f1d8f619ea12d39074a
GitHub Action <action@github.com>
2022-09-26 01:35:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/texousliu/opengitmoji/services/MyProjectService.kt
+++ src/main/kotlin/com/github/texousliu/opengitmoji/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.texousliu.opengitmoji.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.texousliu.opengitmoji.MyBundle
 
 class MyProjectService(project: Project) {
 
