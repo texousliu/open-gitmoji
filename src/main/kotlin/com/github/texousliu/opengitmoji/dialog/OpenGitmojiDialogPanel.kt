78088c6ca19e73579881b72b580273f9130872c2
texousliu <texousliu@gmail.com>
2023-11-08 13:17:14 +0800

    :sparkless: 添加一些说明文档

--- src/main/kotlin/com/github/texousliu/opengitmoji/dialog/OpenGitmojiDialogPanel.kt
+++ src/main/kotlin/com/github/texousliu/opengitmoji/dialog/OpenGitmojiDialogPanel.kt
@@ -4,25 +4,27 @@ package com.github.texousliu.opengitmoji.dialog
 import com.github.texousliu.opengitmoji.context.OpenGitmojiContext
 import com.github.texousliu.opengitmoji.model.RegexTableRowInfo
 import com.github.texousliu.opengitmoji.utils.OpenGitmojiUtils
+import com.intellij.ide.IdeBundle
+import com.intellij.ide.todo.TodoFilter
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.psi.search.TodoPattern
+import com.intellij.ui.DoubleClickListener
 import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBTextField
-import com.intellij.ui.dsl.builder.RightGap
-import com.intellij.ui.dsl.builder.RowLayout
-import com.intellij.ui.dsl.builder.actionListener
-import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.*
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.ui.dsl.gridLayout.VerticalAlign
-import com.intellij.ui.paint.LinePainter2D
 import com.intellij.ui.table.JBTable
 import java.awt.Dimension
+import java.awt.event.MouseEvent
 import javax.swing.JComponent
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableCellEditor
 
 
 @Suppress("DialogTitleCapitalization")
@@ -37,6 +39,13 @@ fun openGitmojiDialogPanel(): DialogPanel {
         }
     }
     jbTable.setShowGrid(false)
+
+    object : DoubleClickListener() {
+        override fun onDoubleClick(event: MouseEvent): Boolean {
+            editSelectedRegex(jbTable)
+            return true
+        }
+    }
 
     return panel {
         group("Input") {
@@ -53,7 +62,7 @@ fun openGitmojiDialogPanel(): DialogPanel {
 
         group("Prompt List") {
             row {
-                label("Not indented row")
+                label("Configure Gitmoji filling expression and generate a filling content list through the expression for users to choose.")
             }.layout(RowLayout.PARENT_GRID)
             row {
                 cell(createPromptListTable(jbTable))
@@ -66,7 +75,46 @@ fun openGitmojiDialogPanel(): DialogPanel {
                         }.resizableColumn()
                         .horizontalAlign(HorizontalAlign.FILL)
                         .verticalAlign(VerticalAlign.FILL)
-            }.layout(RowLayout.PARENT_GRID)
+            }.layout(RowLayout.PARENT_GRID).resizableRow()
+        }
+    }
+}
+
+fun editSelectedRegex(jbTable: JBTable) {
+//    val jbTableModel = jbTable.model as DefaultTableModel
+//    stopEditing(jbTable)
+//    val selectedIndex: Int = jbTable.getSelectedRow()
+//    if (selectedIndex < 0 || selectedIndex >= jbTableModel.getRowCount()) {
+//        return
+//    }
+//    // 获取选择的内容
+//    val
+//    val rowInfo = OpenGitmojiContext.getRegexTableInfoObj().rows.get(selectedIndex)
+//    val dialog = PatternDialog(myPanel, pattern, selectedIndex, myPatterns)
+//    dialog.setTitle(IdeBundle.message("title.edit.todo.pattern"))
+//    if (!dialog.showAndGet()) {
+//        return
+//    }
+//    myPatterns.set(selectedIndex, pattern)
+//    myPatternsModel.fireTableRowsUpdated(selectedIndex, selectedIndex)
+//    myPatternsTable.getSelectionModel().setSelectionInterval(selectedIndex, selectedIndex)
+//    // Update model with patterns
+//    // Update model with patterns
+//    for (i in 0 until myFilters.size()) {
+//        val filter: TodoFilter = myFilters.get(i)
+//        if (filter.contains(sourcePattern)) {
+//            filter.removeTodoPattern(sourcePattern)
+//            filter.addTodoPattern(pattern)
+//            myFiltersModel.fireTableRowsUpdated(i, i)
+//        }
+//    }
+}
+
+fun stopEditing(jbTable: JBTable) {
+    if (jbTable.isEditing()) {
+        val editor: TableCellEditor = jbTable.getCellEditor()
+        if (editor != null) {
+            editor.stopCellEditing()
         }
     }
 }
@@ -92,7 +140,7 @@ fun createPromptListTable(jbTable: JBTable): JComponent {
                 (jbTable.model as DefaultTableModel).removeRow(row)
                 removeRegexTableInfoRow(row)
             }.createPanel()
-//    panel.preferredSize = Dimension(600, 400)
+    panel.preferredSize = Dimension(0, 400)
     return panel
 }
 
@@ -111,11 +159,11 @@ fun removeRegexTableInfoRow(index: Int) {
 class AddRegexDialogWrapper : DialogWrapper(true) {
 
     var enable = JBCheckBox("Enable Regex")
-    var regex = JBTextField()
-    var demo = JBTextField()
+    var regex = JBTextField(30)
+    var demo = JBTextField(30)
 
     init {
-        title = "测试title"
+        title = "Regex Info"
         regex.document.addDocumentListener(object : DocumentListener {
             override fun insertUpdate(e: DocumentEvent?) {
                 generatorDemo(regex.text)
@@ -142,14 +190,23 @@ class AddRegexDialogWrapper : DialogWrapper(true) {
     override fun createCenterPanel(): JComponent {
         return panel {
             row {
-                cell(enable)
+                cell(enable).gap(RightGap.SMALL)
+                // 添加帮助图标
+                contextHelp("Configure whether the regular expression takes effect. Some expressions do not need to take effect in real time, so this configuration item is provided.", "Enable regex help")
             }
-            row {
-                label("Regex")
+            row("Regex: ") {
                 cell(regex).horizontalAlign(HorizontalAlign.FILL)
+                        .comment("""
+                            Prompt list expression configuration. The system provides the following placeholders by default:<br>
+                            #{G}: Fill in gitmoji <br>
+                            #{GU}: Fill in gimoji unicode <br>
+                            #{DESC}: Fill in gitmoji description (English) <br>
+                            #{DESC_CN}: Fill in gitmoji description (Chinese) <br>
+                            #{DATE}: Fill in the current date <br>
+                            #{TIME}: fill in the current time
+                        """.trimIndent())
             }.layout(RowLayout.PARENT_GRID)
-            row {
-                label("Demo")
+            row("Demo: ") {
                 cell(demo).horizontalAlign(HorizontalAlign.FILL)
             }.layout(RowLayout.PARENT_GRID)
         }
