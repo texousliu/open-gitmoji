b6141638c0f0e0950e028fd5ab124d9bb3c9f8f4
liuxiaohua <liuxiaohua@keyfil.com>
2025-12-10 16:33:58 +0800

    ⬆️ ♻️ [2025-12-10] 升级插件版本，修复废弃API调用

--- src/main/kotlin/com/github/texousliu/open/emoji/contributor/OpenEmojiDocCompletionConfidence.kt
+++ src/main/kotlin/com/github/texousliu/open/emoji/contributor/OpenEmojiDocCompletionConfidence.kt
@@ -7,22 +7,21 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.util.elementType
 import com.intellij.util.ThreeState
 
-class OpenEmojiDocCompletionConfidence : CompletionConfidence()
-{
+class OpenEmojiDocCompletionConfidence : CompletionConfidence() {
 
     private val docStart: String = "DOC_"
     private val commentEnd: String = "_COMMENT"
 
-    @Deprecated("兼容 IntelliJ IDEA Ultimate 2024.3 eap (243.16718.32)")
-    override fun shouldSkipAutopopup(contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState {
+    override fun shouldSkipAutopopup(
+        editor: Editor,
+        contextElement: PsiElement,
+        psiFile: PsiFile,
+        offset: Int
+    ): ThreeState {
         val elementType = contextElement.elementType.toString()
         if (elementType.startsWith(docStart) || elementType.endsWith(commentEnd))
             return ThreeState.NO
         return ThreeState.UNSURE
-    }
-
-    fun shouldSkipAutopopup(editor: Editor, contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState {
-        return shouldSkipAutopopup(contextElement, psiFile, offset)
     }
 
 }
